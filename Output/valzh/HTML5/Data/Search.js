b0d2c24ed7741c820bc7b467041e2bde8b838861
Olga <virginia@tut.by>
2021-03-23 22:11:13 +0100

    666

---
+++ Output/valzh/HTML5/Data/Search.js
@@ -0,0 +1,1 @@
+define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/A_Description/10_Konsumpcja_wina.htm'],s:['0','odrzucenia','wyprodukowano'],p:[['0',7],['każdej',18],['pożywek',7],['winem',4]]});
---
+++ Output/valzh/HTML5/Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=637520428675288193'
+});
